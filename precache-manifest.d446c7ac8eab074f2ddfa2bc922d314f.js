399a89bbe076d6afe74c5a96ab225af1048ee8f3
luckidubi <nwodochidubem1@gmail.com>
2019-06-16 16:39:24 +0100

    Updates

---
+++ precache-manifest.d446c7ac8eab074f2ddfa2bc922d314f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb742531c00552edb32ac3aa0c82f39d",
+    "url": "/contact_app/index.html"
+  },
+  {
+    "revision": "3148a004f903ec72e3ba",
+    "url": "/contact_app/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "64723f453df4b66a16b2",
+    "url": "/contact_app/static/css/main.8350e98f.chunk.css"
+  },
+  {
+    "revision": "3148a004f903ec72e3ba",
+    "url": "/contact_app/static/js/2.b0f4c8b7.chunk.js"
+  },
+  {
+    "revision": "64723f453df4b66a16b2",
+    "url": "/contact_app/static/js/main.13e999cb.chunk.js"
+  },
+  {
+    "revision": "9f6df4e2f30a64690f82",
+    "url": "/contact_app/static/js/runtime~main.be27cdec.js"
+  },
+  {
+    "revision": "064a2aa77abce71dd1cc25e85e41e7d3",
+    "url": "/contact_app/static/media/ShadowsIntoLightTwo-Regular.064a2aa7.woff"
+  },
+  {
+    "revision": "08857db1dae4f18c13b0ad922a2c6f91",
+    "url": "/contact_app/static/media/wds052801.08857db1.woff"
+  }
+]);
